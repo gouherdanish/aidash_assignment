a3425eb26fdfb7b79ce1630f6ec58578c8d144d1
Gouher  DANISH <gdanish@alten-india.dir>
2020-06-29 01:21:23 +0530

    comments added

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -4,34 +4,61 @@ class Node:
         self.left = self.right = None
 
 class BinaryProblem:
-    def __init__(self):
-        pass
 
-    def create_tree(self, arr, root, i, n):
+    def create_tree(self, arr, i, n):
+        """
+        Uses recursion to create a tree
+        
+        Input:  arr - user input array taken to form nodes of a tree
+                i - index with respect to input array
+                n - length of input array
+        Output: node - object of type Node representing a binary tree grown out of input array
+        """
+        node = None
         if i < n:
-            root = Node(arr[i])
+            # Creating instance of root node with value
+            node = Node(arr[i])
 
-            # insert left child
-            root.left = self.create_tree(arr, root.left, 2*i+1, n)
+            # inserting left child
+            node.left = self.create_tree(arr, 2*i+1, n)
 
-            # insert right child
-            root.right = self.create_tree(arr, root.right, 2*i+2, n)
+            # inserting right child
+            node.right = self.create_tree(arr, 2*i+2, n)
 
-        return root
+        return node
     
     def tree_are_mirror(self, tree1, tree2): 
         
+        """
+        Uses recursion to check if the tree1 is mirror of tree2
+        
+        Input: Binary trees tree1 and tree2 of object type Node
+        Output: True - if trees are mirror
+                False - if trees are not mirror
+        """
+        
+        # Empty Trees
         if tree1 is None and tree2 is None: 
             return True
         
+        # If tree1 is empty and tree2 is not, then not mirror
         if tree1 is None or tree2 is None: 
             return False
         
+        # Three conditions for trees to be mirror
+        # - 1) root node must be same
+        # - 2) left sub-tree of tree1 must be same as right sub-tree of tree2
+        # - 3) right sub-tree of tree1 must be same as left sub-tree of tree2
         return (tree1.data == tree2.data and 
                 self.tree_are_mirror(tree1.left, tree2.right) and 
                 self.tree_are_mirror(tree1.right , tree2.left)) 
 
     def convert_to_list(self, mystr):
+        """
+        Splits the user input based on space character
+        input: mystr - string
+        output: arr - list of strings
+        """
         arr = mystr.split(" ")
         return arr
 
@@ -40,19 +67,19 @@ if __name__ == "__main__":
     try:
         bp = BinaryProblem()
 
-        print("for printing the tree just write element, space seperated left to right.")
-        print("if there is no data then use hiphen '-'")
+        print("for printing the tree, just enter element separated by space from left to right.")
+        print("if there is no data then use hyphen '-'")
 
         tree1 = input("for tree 1: ")
         tree1 = bp.convert_to_list(tree1)
-        tree1 = bp.create_tree(tree1, None, 0, len(tree1))
+        tree1 = bp.create_tree(tree1, 0, len(tree1))
 
         tree2 = input("for tree 2: ")
         tree2 = bp.convert_to_list(tree2)
-        tree2 = bp.create_tree(tree2, None, 0, len(tree2))
+        tree2 = bp.create_tree(tree2, 0, len(tree2))
 
-        if bp.tree_are_mirror(tree1, tree2): print("tree are mirror")
-        else: print("not mirror")
+        if bp.tree_are_mirror(tree1, tree2): print("Yes, trees are mirror")
+        else: print("No, trees are not mirror")
 
     except Exception as e:
         print(e)
--- matrix_spiral.py
+++ matrix_spiral.py
@@ -3,6 +3,26 @@ class MatrixProblem:
         self.spiral_matrix = []
         
     def run(self, matrix, m, n, i, j):
+        
+        """
+        Function to print the outer elements of input matrix clockwise
+        Start and end indices are adjusted to get the inner matrix
+        Recursive calls print all the elements in clockwise spiral fashion
+        
+        Input : 
+            matrix - two dimensional array of numbers
+            m - end row index
+            n - end column index
+            i - start row index
+            j - start column index
+                    
+        Output :
+            Does not return anything
+            Appends the class attribute `spiral_matrix` with required spiral elements 
+        """
+        
+        
+        # Terminating Condition - If start index become at least equal to end index
         if i >= m or j >=n: return
 
         # print first row
@@ -18,12 +38,12 @@ class MatrixProblem:
             for x in range(n-2, j-1, -1):
                 self.spiral_matrix.append(matrix[m-1][x])
         
-        # print first column if last one is not same
+        # print first column if it is not same as last one
         if n-1 != j:
             for x in range(m-2, i, -1):
                 self.spiral_matrix.append(matrix[x][j])
         
-        # call recursion with the inside matrix
+        # call recursion with the inner matrix
         self.run(matrix, m-1, n-1, i+1, j+1)
 
 if __name__ == "__main__":
@@ -31,20 +51,22 @@ if __name__ == "__main__":
         m = int(input("Enter total no of rows: "))
         n = int(input("Enter total no of columns: "))
 
-        print("enter all the element seperated by space\n")
+        print("Enter all the element seperated by space\n")
         matrix = []
         for i in range(m):
             mystr = input("for row {}: ".format(i+1))
-            mylist = mystr.split(" ")
+            mylist = [int(i) for i in mystr.split(" ")]
             if len(mylist) != n: raise Exception("there should be exactly {} element".format(n))
             
             matrix.append(mylist)
         
+        print("Input :")
         print(matrix)
 
-        # init the class
+        # initialize the class
         mp = MatrixProblem()
         mp.run(matrix, m, n, 0, 0)
+        print("Output :")
         print(mp.spiral_matrix)
         
     except Exception as e:
